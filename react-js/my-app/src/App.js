64eba6431b6632e5568fbbe017dd0440800d494c
Abhisek parida <abhisekparida2022.com>
2026-07-15 12:04:38 +0530

    commit

--- react-js/my-app/src/App.js
+++ react-js/my-app/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 // import About from './screens/About';
 // import Contact from './screens/Contact';
-import Practice from './screens/Practice';
+// import Practice from './screens/Practice';
+// import Register from './screens/Register';
+import Userlist from './screens/Userlist';
 function App() {
   return (
     <div className="App">
       {/* <About/> */}
       {/* <Contact/> */}
-      <Practice/>
+      {/* <Practice/> */}
+      {/* <Register/> */}
+      <Userlist/>
     </div>
   );
 }
--- react-js/my-app/src/components/Title.js
+++ react-js/my-app/src/components/Title.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-const Title = (props) => {
+const Title = ({name,contactno,schoolname,collegename,rollno}) => {
   return (
     <>
-    <h1>this is a good day</h1>
+    <div>
+      <div>
+        <div>
+          <h1 style={{fontSize:50,color:"red"}}>{name}</h1>
+        </div>
+      </div>
+    </div>
+    <h1 style={{fontSize:50,color:"red"}}>{contactno}</h1>
+    <h1 style={{fontSize:50,color:"red"}}>{schoolname}</h1>
+    <h1 style={{fontSize:50,color:"red"}}>{collegename}</h1>
+    <h1 style={{fontSize:50,color:"red"}}>{rollno}</h1>
+    {/* <h1>this is a good day</h1>
     <h2>{props.name}</h2>
-    <h1>{props.demo}</h1>
-    <h3>{props.test}</h3>
+    <h1>{props.demo}</h1> */}
+    {/* <h3>{props.test}</h3> */}
     </>
   )
 }
